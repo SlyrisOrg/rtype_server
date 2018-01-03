714cb64bd75e352458043a26c3593e3f5f10cb11
Doom <clement.doumergue@epitech.eu>
2018-01-03 18:41:01 +0100

    add main and options
    Change-Id: I8ee36f60a5a64d9eb037fa1fba9d6d1c25ff1216

--- Project/main.cpp
+++ Project/main.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
+#include <boost/program_options.hpp>
 
-int main()
+namespace opt = boost::program_options;
+
+int main(int ac, char **av)
 {
-    std::cout << "Hello World" << std::endl;
+    opt::options_description desc("Available options");
+
+    desc.add_options()
+        ("resources", opt::value<std::string>()->default_value("resources"), "The resources directory")
+        ("help", "display this help message");
+
+    opt::variables_map vm;
+
+    try {
+        opt::store(opt::parse_command_line(ac, (const char *const *)av, desc), vm);
+        opt::notify(vm);
+    } catch (const std::exception &e) {
+        std::cerr << e.what() << std::endl;
+        return 1;
+    }
+
+    if (!vm["help"].empty()) {
+        std::cout << desc << std::endl;
+        return 0;
+    }
     return 0;
 }
